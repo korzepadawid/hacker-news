f2ae74c0e8bef293fae8b3f97a8af9ba61ef03cb
korzepadawid <korzepa1337@gmail.com>
2022-07-25 11:37:15 +0200

    feat: added auth sign in dtos

--- src/main/java/io/github/korzepadawid/hackernewsapi/common/projection/AuthCredentials.java
+++ src/main/java/io/github/korzepadawid/hackernewsapi/common/projection/AuthCredentials.java
@@ -1,4 +1,26 @@
 package io.github.korzepadawid.hackernewsapi.common.projection;
 
 public class AuthCredentials {
+
+    private String email;
+    private String password;
+
+    public AuthCredentials() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
 }
--- src/main/java/io/github/korzepadawid/hackernewsapi/common/projection/AuthDetails.java
+++ src/main/java/io/github/korzepadawid/hackernewsapi/common/projection/AuthDetails.java
@@ -1,4 +1,17 @@
 package io.github.korzepadawid.hackernewsapi.common.projection;
 
 public class AuthDetails {
+
+    private String bearerToken;
+
+    public AuthDetails() {
+    }
+
+    public String getBearerToken() {
+        return bearerToken;
+    }
+
+    public void setBearerToken(final String bearerToken) {
+        this.bearerToken = bearerToken;
+    }
 }
